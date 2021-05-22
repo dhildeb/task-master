9475527525837ea27819f2d5c5cb3f0a5359b792
daniel hildebrandt <d.hildebrandt93@gmail.com>
2021-05-22 13:55:48 -0600

    looks

--- app/Controllers/ListsController.js
+++ app/Controllers/ListsController.js
@@ -11,14 +11,14 @@ function _draw() {
     let tasks = ProxyState.todos.filter(t => t.listId == l.id)
     let completed = tasks.filter(t => t.done)
     template += `
-    <div class="col-4 my-5">
+    <div class="col-lg-4 col-md-6 col-12 my-5">
       <div class="card shadow mb-3">
           <div class="card-header"style="background-color:${l.color};">
-              <div class="d-flex justify-content-between text-light">
-                  <p>${l.name}</p>
-                  <i title="delete list" class="fa fa-times" aria-hidden="true" onclick="app.listsController.deleteList('${l.id}')"></i>
+              <div class="d-flex justify-content-center text-light">
+                  <h5 class="flex-grow-1 text-center">${l.name}</h5>
+                  <i title="delete list" class="fa fa-times " aria-hidden="true" onclick="app.listsController.deleteList('${l.id}')"></i>
               </div>
-              <p class="text-light">${completed.length}/${tasks.length}</p>
+              <p class="text-light text-center">${completed.length}/${tasks.length}</p>
           </div>
           <div class="card-body" style="color: ${l.color}; min-height: 300px;">
           `
@@ -27,7 +27,7 @@ function _draw() {
       template += `
                 <li class="card-text p-1 d-flex justify-content-between">
                   <input onclick="app.todosController.done('${t2.id}')" class="align-self-center double" type="checkbox" ${t2.done ? 'checked' : ''}>
-                  <p class="px-2 text-center" style="${t2.done ? 'text-decoration: line-through;' : ''}">${t2.task}</p>
+                  <p class="px-2 text-center text-wrap" style="${t2.done ? 'text-decoration: line-through;' : ''}">${t2.task}</p>
                   <button title="delete task" class="btn btn-transparent" onclick="app.todosController.deleteTodo('${t2.id}')"><i class="fa fa-times fa-lg text-danger" aria-hidden="true"></i></button>
                 </li>
                 `
@@ -36,7 +36,7 @@ function _draw() {
     </div>
           <form onsubmit="app.todosController.addTodo(event,'${l.id}')">
             <div class="d-flex p-3">
-                <input type="text" name="task" placeholder="Add Task..." minlength="3" maxlength="50" required>
+                <input type="text" name="task" placeholder="Add Task..." minlength="3" maxlength="50" class="flex-grow-1" required>
                 <button title="add task" class="btn btn-transparent"><i class="fa fa-plus-square fa-lg text-success" aria-hidden="true"></i>
                 </button>
             </div>
